0c3aab73e0835475af7c8d2dd743a65e78aaed49
El-xAn <hidefinatio123@gmail.com>
2020-11-22 18:28:41 +0400

    calculator

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -2,7 +2,7 @@ function insert(num) {
     document.form.textview.value =
     document.form.textview.value + num;
 }
-function clean() {
+function cl() {
     document.form.textview.value = '';
 }
 function del() {
@@ -15,9 +15,14 @@ function equal() {
         document.form.textview.value = eval(data);
     }
 }
+function sqrt() {
+    let data = document.form.textview.value;
+        if(data) {
+            document.form.textview.value = math.Sqrt(data);
+        }
+}
 function op() {
     let data = document.form.textview.value;
-    document.form.textview.value = data.substring(0, data.length - 1);
 
     if(typeof data[0] !== 'number') {
         document.form.textview.value = '';
